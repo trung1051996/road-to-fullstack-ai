44ef3d1f4c556ed4a95b02e1279a9f4eecd1fa64
nor.nguyen <nor.nguyen@avepoint.com>
2026-07-31 09:52:44 +0700

    update code FastAPI

--- student_app/main.py
+++ student_app/main.py
@@ -1,2 +1,43 @@
-from utils.math_utils import square
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from services.student_service import StudentService
+from models.student import Student
+app = FastAPI()
 
+class StudentCreate(BaseModel):
+    name: str
+    age: int
+    score:float
+
+@app.get("/")
+def say_hello():
+    return {"message": "Hello FastAPI"}
+
+
+@app.get("/students/{name}/age/{age}")
+def get_student(name: str, age: int):
+    return {"name": name, "age": age}
+
+@app.get("/students")
+def get_students():
+    return StudentService().load_students()
+
+@app.post("/students")
+def create_student(student: StudentCreate): #dict/JSON need deserialize to object
+    student_obj = Student(**student.model_dump()) # deserialize to object
+    new_students = StudentService().add_student(student_obj)
+    return new_students
+
+@app.put("/student/{name}")
+def update_student(old_name: str, new_name: str):
+    students = StudentService().update_student(old_name, new_name)
+    if students is None:
+        raise HTTPException(status_code = 404, detail="Student not found")
+    return students
+
+@app.delete("/student/{name}")
+def delete_student(name: str):
+    students = StudentService().remove_student(name)
+    if students is None:
+        raise HTTPException(status_code = 404, detail="Student not found")
+    return students
--- student_app/services/student_service.py
+++ student_app/services/student_service.py
@@ -39,10 +39,10 @@ class StudentService:
             raise ValueError("Student already exists")
         students.append(student)
         self.save_students(students)
+        return students
 
     def update_student(self, old_name, new_name):
         students = self.load_students()
-        
         student = next(
             (s for s in students if s.name == old_name),
             None
@@ -51,21 +51,27 @@ class StudentService:
             student.name = new_name
             self.save_students(students)
             print('🚀 update successfully :',)
+            return students
         else:
-            print("not exist student")
+            return None
 
 
     def remove_student(self, name):
         students = self.load_students()
-        
-        students = [
-            student
-            for student in students
-            if student.name != name
-        ]
-        print("remove successfully", students)
-
-        self.save_students(students)
+        student = next(
+            (s for s in students if s.name == name),
+            None
+        )
+        if student:
+            students = [
+                student
+                for student in students
+                if student.name != name
+            ]
+            self.save_students(students)
+            return students
+        else:
+            return None
 
     def find_student(self, name):
         students = self.load_students()
